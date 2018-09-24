f2201cf8f54546515671f53e5ee7a36337156f07
Rishabh Agrawal <rishabh120296@gmail.com>
2018-09-24 16:37:33 +0530

    removed ports for docker

--- app.config.js
+++ app.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  MONGODB_URL: process.env.MONGODB_URL ? process.env.MONGODB_URL : 'mongodb://mongo:27017/tldm-github-bot',  
+  MONGODB_URL: process.env.MONGODB_URL ? process.env.MONGODB_URL : 'mongodb://mongo/tldm-github-bot',  
 };
